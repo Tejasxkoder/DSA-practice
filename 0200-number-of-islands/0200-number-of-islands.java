be140565f45b3f35f19daa8b8df5bc420a52bd84
Tejas Kumar <141752954+Tejasxkoder@users.noreply.github.com>
2026-07-16 16:57:05 +0530

    3 ms (87.99%) 52.5 MB (33.78%)

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,54 +1,29 @@
 class Solution {
-
     public int numIslands(char[][] grid) {
-
-        int islandCount = 0;
-
-        // Traverse every cell of the grid
-        for (int row = 0; row < grid.length; row++) {
-
-            for (int col = 0; col < grid[0].length; col++) {
-
-                // New island found
-                if (grid[row][col] == '1') {
-
-                    islandCount++;
-
-                    // Visit the complete island
-                    dfs(grid, row, col);
-                }
+        int IslandCount=0;
+        for(int row=0;row<grid.length;row++ ){
+            for(int col=0; col<grid[0].length;col++){
+            if(grid[row][col]=='1'){
+                IslandCount++;
+                dfs(grid,row,col);
             }
         }
-
-        return islandCount;
+    }
+    return IslandCount;
     }
 
-    private void dfs(char[][] grid, int row, int col) {
-
-        // Base Case 1: Outside the grid
-        if (row < 0 || row >= grid.length ||
-            col < 0 || col >= grid[0].length) {
+    private void dfs(char[][] grid,int row,int col){
+        if(row<0||row>=grid.length||col<0||col>=grid[0].length){
             return;
         }
-
-        // Base Case 2: Water or already visited
-        if (grid[row][col] == '0') {
+        if(grid[row][col]=='0'){
             return;
         }
-
-        // Mark current land as visited
-        grid[row][col] = '0';
-
-        // Visit Up
-        dfs(grid, row - 1, col);
-
-        // Visit Down
-        dfs(grid, row + 1, col);
-
-        // Visit Left
-        dfs(grid, row, col - 1);
-
-        // Visit Right
-        dfs(grid, row, col + 1);
+        grid[row][col]='0';
+        
+        dfs(grid,row+1,col);
+        dfs(grid,row-1,col);
+        dfs(grid,row,col+1);
+        dfs(grid,row,col-1);
     }
 }
